597a19d8d998d3ef4875fb3afca4e0f06d4ac0a5
Dgibrilly Manirakiza Mutabazi <23765518@sun.ac.za>
2024-04-06 22:58:14 +0200

    Data retrieved from API and formated

--- Projects/Real Time Data Streaming Pipeline/dags/kafka_stream.py
+++ Projects/Real Time Data Streaming Pipeline/dags/kafka_stream.py
@@ -10,14 +10,43 @@ default_args ={
     'timezone': 'UTC'
 }
 
-def stream_data():
-    import json
+def get_Data():
     import requests
 
     resp = requests.get('https://randomuser.me/api/')
     resp = resp.json()
     resp = resp['results'][0]
-    print(json.dumps(resp, indent=3))  # structures JSON data to make it easily readible
+
+    return resp
+
+
+# formart data revcd rfrom API
+def format_data(res):
+    data = {} #dictionary
+    #extract data from JSON object
+    location = res['location']
+    data['first_name'] = res['name']['first']
+    data['last_name'] = res['name']['last']
+    data['gender'] = res['gender']
+    data['address'] = f"{str(location['street']['number'])} {location['street']['name']}, "\
+                     f"{location['city']},{location['state']},{location['country']}"
+    data['post_code'] = location['postcode']
+    data['email'] = res['email']
+    data['username'] = res['login']['username']
+    data['dob'] = res['dob']['date']
+    data['registered_date'] = res['registered']['date']
+    data['phone'] = res['phone']
+    data['picture'] = res['picture']['medium']
+
+    return data
+
+
+
+def stream_data():
+    import json
+    res = get_Data()
+    res = format_data(res)
+    print(json.dumps(res, indent=3))
 
 #entry point
 # with DAG('user_automation', 
